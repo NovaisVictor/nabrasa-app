7676ddb9df580bf248fac507f7a2e86992436279
RamiroAvl <ramirogustavoavila1@gmail.com>
2024-08-29 13:02:33 -0300

    Prisma Setup

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,1 +1,14 @@
-console.log('server running')
+import fastifyCors from '@fastify/cors'
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.register(fastifyCors)
+
+app.get('/', () => {
+  return 'alguma mensagem'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('http server running')
+})
